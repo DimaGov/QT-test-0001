32f772e0c801d3982888f8d7fdb5ba45ac81cd49
Dima Govorukha <dimagov390@gmail.com>
2019-08-29 18:22:52 +0300

    Comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <QCoreApplication>
 
+// ----------------------------------------------
+// Точка входа в программу
+// ----------------------------------------------
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
